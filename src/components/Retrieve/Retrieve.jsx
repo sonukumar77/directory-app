4abc7301acebdb0645e4dff0739762db901b0c51
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-08-19 14:21:48 +0530

    Update Retrieve.jsx

--- src/components/Retrieve/Retrieve.jsx
+++ src/components/Retrieve/Retrieve.jsx
@@ -1,7 +1,71 @@
+import { useState } from "react";
+import { getDataFromLS } from "../../utils/helper";
+import NF from "./nf.gif";
 import "./Retrieve.css";
 
 const Retrieve = () => {
-  return <div className="retrieve-container">Retrieve</div>;
+  const [input, setInput] = useState("");
+  const [isFindBtnClicked, setIsFindBtnClicked] = useState(false);
+  const [persondata, setPersondata] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+    setIsFindBtnClicked(false);
+  };
+  const handleFind = () => {
+    const filterData = getDataFromLS().filter(
+      (item) => item["Aadhar Number"] === input
+    );
+    setIsFindBtnClicked(true);
+    setPersondata(filterData);
+  };
+
+  return (
+    <div className="retrieve-container">
+      <h2>Retrieve Information</h2>
+      <div className="input-container">
+        <input
+          type="text"
+          className="input"
+          value={input}
+          onChange={handleInputChange}
+        />
+        <button className="btn" onClick={handleFind}>
+          Find
+        </button>
+      </div>
+      {isFindBtnClicked &&
+        (persondata.length > 0 ? (
+          <div className="output">
+            <p>
+              {" "}
+              <span>Name :</span> <span>{persondata[0]["Name"]}</span>
+            </p>
+            <p>
+              {" "}
+              <span>Date of Birth :</span>{" "}
+              <span>{persondata[0]["Date of Birth"]} </span>
+            </p>
+            <p>
+              {" "}
+              <span>Aadhar Number :</span>{" "}
+              <span>{persondata[0]["Aadhar Number"]}</span>
+            </p>
+            <p>
+              {" "}
+              <span>Mobile Number :</span>{" "}
+              <span>{persondata[0]["Mobile Number"]}</span>
+            </p>
+            <p>
+              {" "}
+              <span>Age :</span> <span>{persondata[0]["Age"]}</span>
+            </p>
+          </div>
+        ) : (
+          <img src={NF} alt="not-found-img" />
+        ))}
+    </div>
+  );
 };
 
 export default Retrieve;
